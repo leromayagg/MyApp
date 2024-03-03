d3681efe33e4bf5a263a5eb10f9d23cacdce861f
leromayagg <leromayagg@gmail.com>
2024-03-03 02:58:59 -0300

    feat: create shared text

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,5 +1,8 @@
-import { Text, TouchableOpacityProps } from 'react-native';
+import { TouchableOpacityProps } from 'react-native';
+import { theme } from '../../themes/theme';
 import { ButtonContainer } from './button.style';
+import Text from '../text/Text';
+import { textTypes } from '../input/textTypes';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
@@ -9,7 +12,7 @@ interface ButtonProps extends TouchableOpacityProps {
 const Button = ({ title, margin, ...props }: ButtonProps) => {
   return (
     <ButtonContainer margin={margin} {...props}>
-      <Text>{title}</Text>
+      <Text color={theme.colors.neutralTheme.white}>{title}</Text>
     </ButtonContainer>
   );
 };
---
+++ src/shared/themes/colors.ts
@@ -0,0 +1,7 @@
+export const grayTheme = {
+  gray80: '#B1BB2',
+};
+
+export const neutralTheme = {
+  white: '#ffffff',
+};
